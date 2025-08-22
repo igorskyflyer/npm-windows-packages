ac083efd8588ecfeba04d3dc0e152f4317009b14
igorskyflyer <igor.dvlpr@gmail.com>
2025-08-22 21:28:06 +0200

    Update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,30 +1,30 @@
 // Author: Igor Dimitrijević (@igorskyflyer)
 
 import { assert, describe, suite, test } from 'vitest'
-import { get, has } from '../src/index.mjs'
+import { get, has } from '../src/index.ts'
 
 describe('🧪 Windows Packages tests 🧪', () => {
-	suite('get()', () => {
-		test('should be an array', () => {
-			assert.isArray(get())
-		}) // #1
+  suite('get()', () => {
+    test('should be an array', () => {
+      assert.isArray(get())
+    }) // #1
 
-		test('length should be > 0', () => {
-			assert.isAbove(get().length, 0)
-		})
+    test('length should be > 0', () => {
+      assert.isAbove(get().length, 0)
+    })
 
-		test('should be a string', () => {
-			assert.isString(get()[0])
-		}) // #2
-	})
+    test('should be a string', () => {
+      assert.isString(get()[0])
+    }) // #2
+  })
 
-	suite('has()', () => {
-		test('should be an array', () => {
-			assert.isArray(has(['MicrosoftEdge', 'MSPaint']))
-		}) // #3
+  suite('has()', () => {
+    test('should be an array', () => {
+      assert.isArray(has(['MicrosoftEdge', 'MSPaint']))
+    }) // #3
 
-		test('should be true', () => {
-			assert.isArray(has(['MicrosoftEdge']))
-		}) // #4
-	})
+    test('should be true', () => {
+      assert.isArray(has(['MicrosoftEdge']))
+    }) // #4
+  })
 })
